e5ce5ac013bace27e2a5248545718035524b2901
sao-coding <58339015+sao-coding@users.noreply.github.com>
2022-11-05 09:21:51 +0000

    tv

--- P04_myTemplate/mainsite/views.py
+++ P04_myTemplate/mainsite/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+
+def index(request):
+    return render(request, "index.html", {'msg': 'Hello World!'})
+
+def yt(request, tvno = 0):
+    tv_list = [
+        {'name':'音樂台0', 'tvcode':'jfKfPfyJRdk'},
+        {'name':'網路流行音樂電台', 'tvcode':'wrYF0HX7Kzc'},
+        {'name':'中天新聞', 'tvcode':'_QbRXRnHMVY'},
+        {'name':'公視新聞網', 'tvcode':'fTkozUQXmF8'},
+        {'name':'藍井エイル - 心臓', 'tvcode':'t1JaVrN5mbk'},
+        ]
+    now = datetime.now()
+    tvno = tvno
+    tv = tv_list[tvno]
+    return render(request, "tv.html", locals())
