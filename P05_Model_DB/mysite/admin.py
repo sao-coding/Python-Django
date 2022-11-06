46f2614a986d92325fb7da7f74f086205565aef8
sao-coding <58339015+sao-coding@users.noreply.github.com>
2022-11-06 09:55:31 +0000

    HW5 sqlite

--- P05_Model_DB/mysite/admin.py
+++ P05_Model_DB/mysite/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Maker, PModel, Product, PPhoto
 
 # Register your models here.
+
+class MakerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country')\
+
+class PModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'maker')\
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('nickname', 'pmodel', 'year', 'price')
+
+class PPhotoAdmin(admin.ModelAdmin):
+    list_display = ('description', 'product')
+
+admin.site.register(Maker, MakerAdmin)
+admin.site.register(PModel, PModelAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(PPhoto, PPhotoAdmin)
